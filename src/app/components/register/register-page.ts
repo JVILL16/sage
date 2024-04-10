2391efe1a04bc35e4db18bb291efd6a236fa8db1
JVILL16 <jheremi2015@gmail.com>
2024-04-10 16:48:20 -0500

    added register tiers on top of page and fixed functionailty in between screens

--- src/app/components/register/register-page.ts
+++ src/app/components/register/register-page.ts
@@ -2,24 +2,35 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../../service/service.component';
 import { first } from 'rxjs/operators';
-import { trigger, transition, animate, style } from '@angular/animations';
+import { trigger, transition, animate, style, group, query } from '@angular/animations';
 import { AlertService } from '../../service/alert.service';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/service/auth.service';
+
+
 
 @Component({
   selector: 'register-page',
   templateUrl: './register-page.html',
   styleUrls: ['./register-page.css'],
   animations: [
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('800ms ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      // transition(":leave", [
+      //   animate('500ms ease-out', style({ transform: 'translateX(-100%)' })),
+      // ])
+    ]),
     trigger('fadeIn', [
       transition(':enter', [
         style({ opacity: 0,transform: 'translateY(-100%)' }),
-        animate(1000, style({ opacity: 1,transform: 'translateY(0%)' }))
-      ]),
-      transition(':leave', [
-        animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
-      ])
+        animate(800, style({ opacity: 1,transform: 'translateY(0%)' }))
+       ]),
+      // transition(':leave', [
+      //   animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+      // ])
     ])
   ]
 })
@@ -37,6 +48,14 @@ export class RegisterComponent {
   first_name: string ="";
   last_name: string ="";
   phone: string ="";
+
+
+  sectOne: boolean = false;
+  sectTwo: boolean = false;
+  sectThree : boolean = false;
+  nextHidden: boolean = false;
+  prevHidden: boolean = true;
+
   roles: any = [{id:'179925',value:'Clutch',checked: false},{id:'539978',value:'Grocery',checked: false},{id:'578343',value:'Kickball',checked: false}];
 
   role_submit : any = [];
@@ -97,6 +116,35 @@ export class RegisterComponent {
     this.url = null;
   }
 
+  onNext() {
+    if(!this.sectOne && !this.sectTwo && !this.sectThree){
+      this.sectOne = true;this.prevHidden = false;
+    }
+      
+    else if(this.sectOne && !this.sectTwo && !this.sectThree){
+      this.sectTwo = true;this.nextHidden = true;
+    }
+    else if(this.sectOne && this.sectTwo && !this.sectThree)
+      this.sectThree = true;
+    else
+      console.log("yay");
+  }
+
+  onPrevious() {
+    if(this.sectOne && this.sectTwo && this.sectThree)
+      this.sectThree = false;
+    else if(this.sectOne && this.sectTwo && !this.sectThree){
+      this.sectTwo = false;this.nextHidden = false;
+    }
+    else if(this.sectOne && !this.sectTwo && !this.sectThree){
+      this.sectOne = false;this.prevHidden = true;
+    }
+      
+    else
+      console.log("yay");
+  }
+
+  
   // get email() { return this.angForm.get('email'); }
   // get password() { return this.angForm.get('password'); }
   // get username() { return this.angForm.get('username'); }
