fb469592bd89d5746068a67ea92261055d32b5ab
JVILL16 <jheremi2015@gmail.com>
2024-07-31 16:41:42 -0500

    alos forgot to add that i changed buildTarget warning so now it wont show it anymore and fix some errors that came up in the clutch page

--- src/app/components/clutch/clutch-page.ts
+++ src/app/components/clutch/clutch-page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/service.component';
 import { User } from '../users/user';
 import { ClutchService } from 'src/app/service/helpers/clutch.service';
+import { AlertService } from 'src/app/service/helpers/alert.service';
 
 @Component({
   selector: 'clucth-page',
@@ -33,29 +34,29 @@ export class ClutchComponent implements OnInit {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
-  constructor(private clutch: ClutchService) { }
+  constructor(private clutch: ClutchService, private alertService:AlertService) { }
  
   ngOnInit() {
     this.clutch.getClutchData('contact').subscribe(
       (data:any) => {
         //this.roster = data;
-        this.captains = data.filter(this.tierOne);
-        this.veterans = data.filter(this.tierTwo);
-        this.incomers = data.filter(this.tierThree);
-        this.practice = data.filter(this.tierFour);
-        console.log(this.captains); // Handle the data as needed
+        this.captains = data?.data.filter(this.tierOne);
+        this.veterans = data?.data.filter(this.tierTwo);
+        this.incomers = data?.data.filter(this.tierThree);
+        this.practice = data?.data.filter(this.tierFour);
+        //console.log(this.captains); // Handle the data as needed
       },
       (error:any) => {
-        console.error('Error fetching Google Sheets data:', error);
+        this.alertService.error('Error fetching Google Sheets data:', error?.message)
       }
     );;
     this.clutch.getClutchData('tourney').subscribe(
       (data:any) => {
-        this.tournaments = data;
-        console.log(this.tournaments); // Handle the data as needed
+        this.tournaments = data?.data;
+        //console.log(this.tournaments); // Handle the data as needed
       },
       (error:any) => {
-        console.error('Error fetching Google Sheets data:', error);
+        this.alertService.error('Error fetching Google Sheets data:', error?.message)
       }
     );;
     
